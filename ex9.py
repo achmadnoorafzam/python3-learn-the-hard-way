43cbd19ffbd723538446b604fd5e68aacf02ae14
achmadnoorafzam <achmadnoorafzam89@gmail.com>
2025-06-18 21:12:44 +0800

    Add ex9.py

---
+++ ex9.py
@@ -0,0 +1,17 @@
+# Printing, Printing, Printing
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print(f"here are the the 7 days of the week: {days}") # Using f-string
+
+print("Here are the months:\n", months)
+print(f"Here are the months using f-string:\n{months}")
+
+print("""
+There's something going on here.
+With the threee double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+      """)
