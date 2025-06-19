80e05dc96832166781d87c73cf4ab27b14c3da50
achmadnoorafzam <achmadnoorafzam89@gmail.com>
2025-06-19 20:59:25 +0800

    Add ex11.py

--- ex10.py
+++ ex10.py
@@ -1,3 +1,5 @@
+# Tab, New Line and Backslash
+
 tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
---
+++ ex11.py
@@ -0,0 +1,15 @@
+# Asking Questions
+
+print("What is your name?", end='==>')
+name = input()
+
+print("How old are you?", end=' | ')
+age = input()
+
+print("How tall are you?", end=' ')
+height = input()
+
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, Your name is {name}, age is {age}, {height} tall and {weight} heavy.")
